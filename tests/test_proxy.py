c4def3155b1e3a5a9c3ecf91d1841ffd6948a0ec
mercury-systems <leondexter692@gmail.com>
2026-08-29 00:33:22 +0100

    fix: encapsulate proxy tests in single asyncio event loop to avoid lock corruption

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -7,18 +7,22 @@ from stealth_scraper.proxy import ProxyPool, ProxyExhaustedError
 
 class TestProxyPool:
     def test_rotation(self):
-        pool = ProxyPool(["http://p1:8080", "http://p2:8080"])
-        p1 = asyncio.run(pool.next())
-        p2 = asyncio.run(pool.next())
-        p3 = asyncio.run(pool.next())
-        assert p1 == p3
+        async def run():
+            pool = ProxyPool(["http://p1:8080", "http://p2:8080"])
+            p1 = await pool.next()
+            p2 = await pool.next()
+            p3 = await pool.next()
+            assert p1 == p3
+        asyncio.run(run())
 
     def test_mark_failed(self):
-        pool = ProxyPool(["http://p1:8080"], max_failures=2)
-        pool.mark_failed("http://p1:8080")
-        pool.mark_failed("http://p1:8080")
-        with pytest.raises(ProxyExhaustedError):
-            asyncio.run(pool.next())
+        async def run():
+            pool = ProxyPool(["http://p1:8080"], max_failures=2)
+            pool.mark_failed("http://p1:8080")
+            pool.mark_failed("http://p1:8080")
+            with pytest.raises(ProxyExhaustedError):
+                await pool.next()
+        asyncio.run(run())
 
     def test_stats(self):
         pool = ProxyPool(["http://p1:8080"])
